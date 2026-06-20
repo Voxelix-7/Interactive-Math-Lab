8eb2a45056800dff4d6f10b5adcd93d99d74f2d7
Voxelix-7 <ayatrshaalan15@gmail.com>
2026-06-20 21:29:02 +0300

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -6,7 +6,6 @@ import { congruenceModule } from './modules/congruence.js';
 import { polygonModule } from './modules/polygons.js';
 import { circleModule } from './modules/circles.js';
 import { subtendedAnglesModule } from './modules/subtendedAngles.js';
-
 import { setCurrentModule } from './state.js';
 
 // App Initialization 
