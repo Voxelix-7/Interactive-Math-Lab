ab41dd314fa8ebebea387af9f13244f3c3e004c8
Voxelix-7 <ayatrshaalan15@gmail.com>
2026-07-14 16:15:35 +0300

    Update sectorSegment.js

--- Js/modules/sectorSegment.js
+++ Js/modules/sectorSegment.js
@@ -136,7 +136,7 @@ export const sectorSegmentModule = {
     ctx.globalAlpha = 0.4;
     ctx.fill();
     ctx.globalAlpha = 1;
-    ctx.strokeStyle = "#f39c12";
+    ctx.strokeStyle = "red";
     ctx.lineWidth = 3;
     ctx.stroke();
     const p = pointOnCircle(cx, cy, drawRadius, end); drawDragger(p);
