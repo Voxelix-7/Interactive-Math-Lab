0656bd295eec1678ce2929ce38aa969df0272807
Voxelix-7 <ayatrshaalan15@gmail.com>
2026-07-14 15:57:49 +0300

    Update sectorSegment.js

--- Js/modules/sectorSegment.js
+++ Js/modules/sectorSegment.js
@@ -124,13 +124,13 @@ export const sectorSegmentModule = {
     const deg = labState.sector.angleDeg;
     const end = toRad(deg);
     ctx.beginPath();
-    ctx.arc(cx, cy, r, 0, TAU);
+    ctx.arc(cx, cy, 110, 0, TAU);
     ctx.strokeStyle = "#bcaaa4aa";
     ctx.lineWidth = 2;
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(cx, cy);
-    ctx.arc(cx, cy, r, 0, end);
+    ctx.arc(cx, cy, 110, 0, end);
     ctx.closePath();
     ctx.fillStyle = "#f39c12";
     ctx.globalAlpha = 0.4;
@@ -139,7 +139,7 @@ export const sectorSegmentModule = {
     ctx.strokeStyle = "#f39c12";
     ctx.lineWidth = 3;
     ctx.stroke();
-    const p = pointOnCircle(cx, cy, r, end); drawDragger(p);
+    const p = pointOnCircle(cx, cy, 110, end); drawDragger(p);
         
     this.updateStats();
     }
